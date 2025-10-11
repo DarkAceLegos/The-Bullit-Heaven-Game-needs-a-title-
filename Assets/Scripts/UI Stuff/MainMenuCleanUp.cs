fff884dbef25b42f22f609bcb231375764baf787
DarkAceLegos <DarkAceLegos@gmail.com>
2025-10-11 18:47:11 -0400

    cleanupstuff

---
+++ Assets/Scripts/UI Stuff/MainMenuCleanUp.cs
@@ -0,0 +1,11 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class MainMenuCleanUp : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(NetworkManager.Singleton != null)
+            Destroy(NetworkManager.Singleton.gameObject);
+    }
+}
