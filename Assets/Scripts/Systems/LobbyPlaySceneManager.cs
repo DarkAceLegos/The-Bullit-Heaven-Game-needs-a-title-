4c40665f2aec2fc2d71ebeeb1e465660bb742a38
DarkAceLegos <DarkAceLegos@gmail.com>
2025-12-15 15:58:02 -0500

    Update LobbyPlaySceneManager.cs

--- Assets/Scripts/Systems/LobbyPlaySceneManager.cs
+++ Assets/Scripts/Systems/LobbyPlaySceneManager.cs
@@ -1,23 +1,38 @@
-using System;
+using System.Collections.Generic;
 using Unity.Netcode;
-using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LobbyPlaySceneManager : NetworkBehaviour
 {
     [SerializeField] Transform PlayerPrefabForLobbyPlayScene;
-    //[SerializeField] int playerIndex = 0;
 
-    private void Start()
+    public override void OnNetworkSpawn()
     {
-        GameMultiplayerConnectionAppoval.Instance.OnPlayerDataNetworkChanged += GameMultiplayerConnectionAppoval_OnPlayerDataNetworkChanged;
+        Debug.Log("Spawned");
+        NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneManager_OnLoadEventCompleted;
+        NetworkManager.OnConnectionEvent += NetworkManager_OnConnectionEvent;
     }
 
-    private void GameMultiplayerConnectionAppoval_OnPlayerDataNetworkChanged(object sender, EventArgs e)
+    private void NetworkManager_OnConnectionEvent(NetworkManager arg1, ConnectionEventData arg2)
+    {
+        if (!IsServer)
+        {
+            Debug.Log("trying to spawn player");
+            SpawnPlayerInLobbyRPC(NetworkManager.Singleton.LocalClientId);
+        }
+    }
+
+    private void SceneManager_OnLoadEventCompleted(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
     {
         Debug.Log("trying to spawn player");
+        SpawnPlayerInLobbyRPC(NetworkManager.Singleton.LocalClientId);
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SpawnPlayerInLobbyRPC(ulong playerId)
+    {
         Transform playerTransform = Instantiate(PlayerPrefabForLobbyPlayScene);
-        //PlayerData playerData = GameMultiplayerConnectionAppoval.Instance.GetPlayerDataFromPlayerIndex(playerIndex);
-        playerTransform.GetComponent<NetworkObject>().SpawnAsPlayerObject(NetworkManager.Singleton.LocalClientId, true);
+        playerTransform.GetComponent<NetworkObject>().SpawnAsPlayerObject(playerId, true);
     }
 }
