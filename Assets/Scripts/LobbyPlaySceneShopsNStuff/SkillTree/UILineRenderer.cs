0880f7d8d22d136ff239ab02dbcd932b244c22ac
DarkAceLegos <DarkAceLegos@gmail.com>
2026-05-24 06:00:28 -0400

    lines test

---
+++ Assets/Scripts/LobbyPlaySceneShopsNStuff/SkillTree/UILineRenderer.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILineRenderer : Graphic
+{
+    [SerializeField] public List<Vector2> points;
+
+    [SerializeField] public float thickness = 10f;
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+
+        
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector2 point = points[i];
+
+            DrawVerticesForPoint(point, vh);
+        }
+
+        for (int i = 0;i < points.Count-1; i++)
+        {
+            int index = i * 2;
+            vh.AddTriangle(index + 0, index + 1, index + 3);
+            vh.AddTriangle(index + 3, index + 2, index + 0);
+        }
+    }
+
+    private void DrawVerticesForPoint(Vector2 point, VertexHelper vh)
+    {
+        UIVertex vertex = UIVertex.simpleVert;
+        vertex.color = color;
+
+        vertex.position = new Vector3(-thickness / 2, 0);
+        vertex.position += new Vector3(point.x, point.y);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(thickness / 2, 0);
+        vertex.position += new Vector3(point.x, point.y);
+        vh.AddVert(vertex);
+    }
+}
