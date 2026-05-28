a5f312f50b626ff9bba4acb4685636a54f9dbf70
DarkAceLegos <DarkAceLegos@gmail.com>
2026-05-28 19:29:08 -0400

    lines working

--- Assets/Scripts/LobbyPlaySceneShopsNStuff/SkillTree/ConnectionLine.cs
+++ Assets/Scripts/LobbyPlaySceneShopsNStuff/SkillTree/ConnectionLine.cs
@@ -9,13 +9,23 @@ public class ConnectionLine : MonoBehaviour
 
     private void Start()
     {
-        transform.position = startNode.transform.position;
-        transform.rotation = Quaternion.Euler(0,0, GetAngle(startNode.transform.position, endNode.transform.position));
+        transform.position = (startNode.transform.position + endNode.transform.position) / 2;
+        transform.rotation = Quaternion.Euler(0, 0, GetAngle(startNode.transform.position, endNode.transform.position) - 90);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(width, 2 * Vector2.Distance(startNode.transform.position, endNode.transform.position));
+        //transform.LookAt(endNode.transform);
+
+        //Debug.Log(GetAngle(startNode.transform.position, endNode.transform.position));
+    }
+
+    private void Update()
+    {
+        transform.position = (startNode.transform.position + endNode.transform.position) / 2;
+        transform.rotation = Quaternion.Euler(0, 0, GetAngle(startNode.transform.position, endNode.transform.position) - 90);
         GetComponent<RectTransform>().sizeDelta = new Vector2(width, 2 * Vector2.Distance(startNode.transform.position, endNode.transform.position));
     }
 
     public float GetAngle(Vector2 me, Vector2 target)
     {
-        return (float)(Mathf.Atan2(target.y - me.y, target.x - me.y) * (180 / Mathf.PI));
+        return (float)(Mathf.Atan2(target.y - me.y, target.x - me.x) * (180 / Mathf.PI));
     }
 }
